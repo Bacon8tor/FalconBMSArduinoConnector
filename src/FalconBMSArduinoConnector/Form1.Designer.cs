d6c49316a63c9d09396cf5d09132ff4282a30d89
Bacon8tor <droidstealer4allah@gmail.com>
2025-07-11 11:31:44 -0700

    initital
    ability to read the data from BMS and display on the UI.

---
+++ src/FalconBMSArduinoConnector/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace FalconBMSArduinoConnector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.falconRunning = new System.Windows.Forms.CheckBox();
+            this.masterCaution_check = new System.Windows.Forms.CheckBox();
+            this.seatArmed_check = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gearLightFront_check = new System.Windows.Forms.CheckBox();
+            this.gearLightLeft_check = new System.Windows.Forms.CheckBox();
+            this.gearLightRight_check = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.falconBuild_text = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // falconRunning
+            // 
+            this.falconRunning.AutoSize = true;
+            this.falconRunning.Location = new System.Drawing.Point(3, 14);
+            this.falconRunning.Name = "falconRunning";
+            this.falconRunning.Size = new System.Drawing.Size(101, 17);
+            this.falconRunning.TabIndex = 0;
+            this.falconRunning.Text = "Falcon Running";
+            this.falconRunning.UseVisualStyleBackColor = true;
+            // 
+            // masterCaution_check
+            // 
+            this.masterCaution_check.AutoSize = true;
+            this.masterCaution_check.Location = new System.Drawing.Point(23, 46);
+            this.masterCaution_check.Name = "masterCaution_check";
+            this.masterCaution_check.Size = new System.Drawing.Size(94, 17);
+            this.masterCaution_check.TabIndex = 1;
+            this.masterCaution_check.Text = "MasterCaution";
+            this.masterCaution_check.UseVisualStyleBackColor = true;
+            // 
+            // seatArmed_check
+            // 
+            this.seatArmed_check.AutoSize = true;
+            this.seatArmed_check.Location = new System.Drawing.Point(23, 78);
+            this.seatArmed_check.Name = "seatArmed_check";
+            this.seatArmed_check.Size = new System.Drawing.Size(81, 17);
+            this.seatArmed_check.TabIndex = 2;
+            this.seatArmed_check.Text = "Seat Armed";
+            this.seatArmed_check.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gearLightRight_check);
+            this.panel1.Controls.Add(this.gearLightLeft_check);
+            this.panel1.Controls.Add(this.gearLightFront_check);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(144, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(91, 69);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gear";
+            // 
+            // gearLightFront_check
+            // 
+            this.gearLightFront_check.AutoSize = true;
+            this.gearLightFront_check.Location = new System.Drawing.Point(40, 20);
+            this.gearLightFront_check.Name = "gearLightFront_check";
+            this.gearLightFront_check.Size = new System.Drawing.Size(15, 14);
+            this.gearLightFront_check.TabIndex = 1;
+            this.gearLightFront_check.UseVisualStyleBackColor = true;
+            // 
+            // gearLightLeft_check
+            // 
+            this.gearLightLeft_check.AutoSize = true;
+            this.gearLightLeft_check.Location = new System.Drawing.Point(19, 44);
+            this.gearLightLeft_check.Name = "gearLightLeft_check";
+            this.gearLightLeft_check.Size = new System.Drawing.Size(15, 14);
+            this.gearLightLeft_check.TabIndex = 2;
+            this.gearLightLeft_check.UseVisualStyleBackColor = true;
+            // 
+            // gearLightRight_check
+            // 
+            this.gearLightRight_check.AutoSize = true;
+            this.gearLightRight_check.Location = new System.Drawing.Point(59, 44);
+            this.gearLightRight_check.Name = "gearLightRight_check";
+            this.gearLightRight_check.Size = new System.Drawing.Size(15, 14);
+            this.gearLightRight_check.TabIndex = 3;
+            this.gearLightRight_check.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.falconBuild_text);
+            this.panel2.Controls.Add(this.falconRunning);
+            this.panel2.Location = new System.Drawing.Point(588, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 94);
+            this.panel2.TabIndex = 4;
+            // 
+            // falconBuild_text
+            // 
+            this.falconBuild_text.AutoSize = true;
+            this.falconBuild_text.Location = new System.Drawing.Point(23, 34);
+            this.falconBuild_text.Name = "falconBuild_text";
+            this.falconBuild_text.Size = new System.Drawing.Size(33, 13);
+            this.falconBuild_text.TabIndex = 1;
+            this.falconBuild_text.Text = "Build:";
+            this.falconBuild_text.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.seatArmed_check);
+            this.Controls.Add(this.masterCaution_check);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox falconRunning;
+        private System.Windows.Forms.CheckBox masterCaution_check;
+        private System.Windows.Forms.CheckBox seatArmed_check;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox gearLightRight_check;
+        private System.Windows.Forms.CheckBox gearLightLeft_check;
+        private System.Windows.Forms.CheckBox gearLightFront_check;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label falconBuild_text;
+    }
+}
+
---
+++ src/FalconBMSArduinoConnector/Form1.cs
@@ -0,0 +1,76 @@
+﻿using F4SharedMem;
+using F4SharedMem.Headers;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+
+namespace FalconBMSArduinoConnector
+{
+    public partial class Form1 : Form
+    {
+        private Reader bmsReader = new Reader();
+        private Timer falconCheckTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            CheckFalconStatus(); // first check
+
+            falconCheckTimer = new Timer();
+            falconCheckTimer.Interval = 1000;
+            falconCheckTimer.Tick += (s, args) => CheckFalconStatus();
+            falconCheckTimer.Start();
+        }
+
+        private void CheckFalconStatus()
+        {
+            bool isRunning = Process.GetProcessesByName("Falcon BMS").Length > 0;
+
+            falconRunning.Checked = isRunning;
+            falconRunning.Text = isRunning ? "Falcon Running" : "Falcon Not Running";
+            falconRunning.ForeColor = isRunning ? Color.Green : Color.Red;
+
+            Console.WriteLine(isRunning ? "Falcon is running" : "Falcon is NOT running");
+
+            if (isRunning)
+            {
+                var data = bmsReader.GetCurrentData();
+                falconBuild_text.Text = "Build: " + data.BMSVersionMajor.ToString() + "." +
+                                       data.BMSVersionMinor.ToString() + "." +                                       
+                                       data.BMSBuildNumber.ToString();
+                falconBuild_text.Visible = true;
+                if (data != null)
+                {
+                    // Example: check if Master Caution light is on
+                    bool masterCaution = (data.lightBits & (uint)LightBits.MasterCaution) != 0;
+                    masterCaution_check.Checked = masterCaution;
+                    Console.WriteLine("Master Caution: " + (masterCaution ? "ON" : "OFF"));
+
+                    // You can check others similarly:
+                    bool seatArm = (data.lightBits2 & (uint)LightBits2.SEAT_ARM) != 0;
+                    seatArmed_check.Checked = seatArm;
+                    Console.WriteLine((uint)LightBits2.SEAT_ARM);
+                    Console.WriteLine("Seat Armed: " + (seatArm ? "YES" : "NO"));
+
+                    bool gearDown_L = (data.lightBits3 & (uint)LightBits3.LeftGearDown) != 0;
+                    //add checkbox for Left Gear Down in your form
+                    gearLightLeft_check.Checked = gearDown_L;
+                    gearLightLeft_check.BackColor = gearDown_L ? Color.Green : Color.Red;
+                    Console.WriteLine("Left Gear Down: " + (gearDown_L ? "YES" : "NO"));
+
+                    
+                }
+            }
+        }
+
+
+    }
+}
