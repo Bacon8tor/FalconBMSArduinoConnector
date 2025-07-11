068c77b81d3199158ac9e70494dc9eeb055610db
Bacon8tor <droidstealer4allah@gmail.com>
2025-07-11 11:53:32 -0700

    added most lightbits

--- src/FalconBMSArduinoConnector/Form1.Designer.cs
+++ src/FalconBMSArduinoConnector/Form1.Designer.cs
@@ -32,14 +32,17 @@
             this.masterCaution_check = new System.Windows.Forms.CheckBox();
             this.seatArmed_check = new System.Windows.Forms.CheckBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.gearLightFront_check = new System.Windows.Forms.CheckBox();
-            this.gearLightLeft_check = new System.Windows.Forms.CheckBox();
             this.gearLightRight_check = new System.Windows.Forms.CheckBox();
+            this.gearLightLeft_check = new System.Windows.Forms.CheckBox();
+            this.gearLightFront_check = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.falconBuild_text = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tf_check = new System.Windows.Forms.CheckBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // falconRunning
@@ -55,7 +58,7 @@
             // masterCaution_check
             // 
             this.masterCaution_check.AutoSize = true;
-            this.masterCaution_check.Location = new System.Drawing.Point(23, 46);
+            this.masterCaution_check.Location = new System.Drawing.Point(3, 3);
             this.masterCaution_check.Name = "masterCaution_check";
             this.masterCaution_check.Size = new System.Drawing.Size(94, 17);
             this.masterCaution_check.TabIndex = 1;
@@ -65,7 +68,7 @@
             // seatArmed_check
             // 
             this.seatArmed_check.AutoSize = true;
-            this.seatArmed_check.Location = new System.Drawing.Point(23, 78);
+            this.seatArmed_check.Location = new System.Drawing.Point(278, 46);
             this.seatArmed_check.Name = "seatArmed_check";
             this.seatArmed_check.Size = new System.Drawing.Size(81, 17);
             this.seatArmed_check.TabIndex = 2;
@@ -78,28 +81,19 @@
             this.panel1.Controls.Add(this.gearLightLeft_check);
             this.panel1.Controls.Add(this.gearLightFront_check);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(144, 26);
+            this.panel1.Location = new System.Drawing.Point(431, 26);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(91, 69);
             this.panel1.TabIndex = 3;
             // 
-            // label1
+            // gearLightRight_check
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 4);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Gear";
-            // 
-            // gearLightFront_check
-            // 
-            this.gearLightFront_check.AutoSize = true;
-            this.gearLightFront_check.Location = new System.Drawing.Point(40, 20);
-            this.gearLightFront_check.Name = "gearLightFront_check";
-            this.gearLightFront_check.Size = new System.Drawing.Size(15, 14);
-            this.gearLightFront_check.TabIndex = 1;
-            this.gearLightFront_check.UseVisualStyleBackColor = true;
+            this.gearLightRight_check.AutoSize = true;
+            this.gearLightRight_check.Location = new System.Drawing.Point(59, 44);
+            this.gearLightRight_check.Name = "gearLightRight_check";
+            this.gearLightRight_check.Size = new System.Drawing.Size(15, 14);
+            this.gearLightRight_check.TabIndex = 3;
+            this.gearLightRight_check.UseVisualStyleBackColor = true;
             // 
             // gearLightLeft_check
             // 
@@ -110,14 +104,23 @@
             this.gearLightLeft_check.TabIndex = 2;
             this.gearLightLeft_check.UseVisualStyleBackColor = true;
             // 
-            // gearLightRight_check
+            // gearLightFront_check
             // 
-            this.gearLightRight_check.AutoSize = true;
-            this.gearLightRight_check.Location = new System.Drawing.Point(59, 44);
-            this.gearLightRight_check.Name = "gearLightRight_check";
-            this.gearLightRight_check.Size = new System.Drawing.Size(15, 14);
-            this.gearLightRight_check.TabIndex = 3;
-            this.gearLightRight_check.UseVisualStyleBackColor = true;
+            this.gearLightFront_check.AutoSize = true;
+            this.gearLightFront_check.Location = new System.Drawing.Point(40, 20);
+            this.gearLightFront_check.Name = "gearLightFront_check";
+            this.gearLightFront_check.Size = new System.Drawing.Size(15, 14);
+            this.gearLightFront_check.TabIndex = 1;
+            this.gearLightFront_check.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gear";
             // 
             // panel2
             // 
@@ -138,15 +141,34 @@
             this.falconBuild_text.Text = "Build:";
             this.falconBuild_text.Visible = false;
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.tf_check);
+            this.panel3.Controls.Add(this.masterCaution_check);
+            this.panel3.Location = new System.Drawing.Point(12, 26);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 5;
+            // 
+            // tf_check
+            // 
+            this.tf_check.AutoSize = true;
+            this.tf_check.Location = new System.Drawing.Point(3, 27);
+            this.tf_check.Name = "tf_check";
+            this.tf_check.Size = new System.Drawing.Size(39, 17);
+            this.tf_check.TabIndex = 2;
+            this.tf_check.Text = "TF";
+            this.tf_check.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.seatArmed_check);
-            this.Controls.Add(this.masterCaution_check);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -154,6 +176,8 @@
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,6 +195,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label falconBuild_text;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.CheckBox tf_check;
     }
 }
 
